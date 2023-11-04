68dd7c3abc3576260deb9db50bb9e438fc752668
Brendan Wilhelmsen <brendanrayw@gmail.com>
2023-11-03 21:52:10 -0700

    Add endpoint to calculate the price based on amount of cargo and distance

--- main.py
+++ main.py
@@ -19,3 +19,14 @@ def read_item(item_id: int, q: Union[str, None] = None):
 def get_order(order_details: order.Order):
     order.save_order(order_details)
     return f"order saved\n"
+
+@app.get("/price")
+def calculate_price(miles: int,
+                    pallets: int = 0,
+                    packages: int = 0):
+    markup = 0.5
+    pallet_cost_per_mile = 0.0683053788345865
+    package_cost_per_mile = 0.0192470664936441
+    pallet_cost = pallets * pallet_cost_per_mile * miles
+    package_cost = packages * package_cost_per_mile * miles
+    return (package_cost + pallet_cost) * (1 + markup)
