b3774a031b2a420b575d3f5d6c9c30c9fffc39a6
Brendan Wilhelmsen <brendanrayw@gmail.com>
2023-11-14 11:44:17 -0800

    adding triangle calculation and a way to validate if an order can be added to an existing route

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import json
 from typing import Union
+from typing import Tuple
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 
-import order
+import route_evaluator
+from order import Order
 import price_calculator
 
 app = FastAPI()
@@ -17,12 +20,15 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.post("/orders", response_class=PlainTextResponse)
-def get_order(order_details: order.Order):
-    order.save_order(order_details)
+def get_order(order: Order):
+    order.save_order()
     return f"order saved\n"
 
 @app.get("/price")
-def calculate_price(miles: int,
-                    pallets: int = 0,
-                    packages: int = 0) -> float:
+def calculate_price(miles: int, pallets: int = 0, packages: int = 0) -> float:
     return price_calculator.calculate(miles, pallets, packages)
+
+@app.post("/validate_order")
+def get_routes(order: Order) -> bool:
+    return route_evaluator.possible_route(order)
+    
---
+++ route_evaluator.py
@@ -0,0 +1,27 @@
+import json
+from triangle import Triangle
+from geopy import distance
+from order import Order
+from typing import Tuple
+
+def possible_route(order: Order) -> bool:
+	# eventually this will be a db call to get a list of orders
+	start = order.location_origin
+	end = order.location_destination
+
+	# perform this check on each route once we have a seeded DB
+	route = json.load(open('order.json'))
+	pickup = route['location_origin']
+	dropoff = route['location_destination']
+
+	valid_pickup = Triangle(
+		start_final = distance.distance(start, end).mi,
+		start_point = distance.distance(start, pickup).mi,
+		final_point = distance.distance(end, pickup).mi).height < 2
+
+	valid_dropoff = Triangle(
+		start_final = distance.distance(start, end).mi,
+		start_point = distance.distance(start, dropoff).mi,
+		final_point = distance.distance(end, dropoff).mi).height < 2
+	
+	return valid_pickup and valid_dropoff
